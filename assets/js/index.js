91e3458baf4cbf6bcecefb5582c701b5939f48c5
Arsentii <tatarenkoarsen@gmail.com>
2021-08-30 13:55:51 +0300

    added arrey

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,21 +22,21 @@ let createTable = () => {
 createTable();
 
 // task 6
-// let multipleValue = () => {
-//   let userVaslue = prompt("first value"),
-//     num = prompt("second value"),
-//     result = Math.round(userVaslue / num) * num,
-//     p = document.createElement("p");
-//   p.innerHTML = "Task 6";
-//   body.appendChild(p);
-//   return result;
-// };
+let multipleValue = () => {
+  let userVaslue = prompt("first value - user value"),
+    num = prompt("second value - num"),
+    result = Math.round(userVaslue / num) * num,
+    p = document.createElement("p");
+  p.innerHTML = "Task 6";
+  body.appendChild(p);
+  return result;
+};
 
-// let p1 = document.createElement("p");
+let p1 = document.createElement("p");
 
-// let multipleResult = multipleValue();
-// body.appendChild(p1);
-// p1.innerText = multipleResult;
+let multipleResult = multipleValue();
+body.appendChild(p1);
+p1.innerText = `Ближайшее число кратное num =  ${multipleResult}`;
 
 // task 5
 console.log("Task 5");
@@ -48,14 +48,14 @@ let multNumbers = (lim1, lim2) => {
   }
 };
 
-let answer = multNumbers(2, 9);
+multNumbers(2, 9);
 
 // Task 4
 console.log("Task 4");
 let limNumbers = () => {
   let lim1 = 1,
     lim2 = 30,
-    num = 2,
+    num = 3,
     result;
 
   for (; lim1 < lim2; lim1++) {
@@ -80,6 +80,7 @@ let equation = () => {
 equation();
 
 // Task 2
+console.log("Task 2");
 let pizzaCost = () => {
   let weight = 900,
     price = 135,
@@ -90,4 +91,66 @@ let pizzaCost = () => {
   }
   return sum;
 };
-alert(pizzaCost());
+console.log(pizzaCost());
+// Task arrey
+console.log("Task arrey");
+let myArr1 = () => {
+  let arr1 = [1, 2, 3, 4, 5, 6, 7, "home", "work"],
+    arr2 = [4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, "home", "done"];
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) === -1) {
+      arr1.push(arr2[i]);
+    }
+  }
+
+  return arr1;
+};
+
+let myArr2 = () => {
+  let arr3 = [1, 2, 2, 3, 4, 3, 4, 5, 6, 7, "home", "work"];
+  arr4 = [1, 2, 3, 4, 5, 5, 5, 5, 6, 7, 8, 9, "home", "done"];
+
+  arr4.forEach((elem) => arr3.push(elem));
+  arr3 = [...new Set(arr3)];
+
+  return arr3;
+};
+
+console.log(myArr1());
+console.log(myArr2());
+
+let myArr3 = () => {
+  let arr1 = [1, 2, 3, 4, 5, 6, 7],
+    arr2 = [4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15];
+
+  let arr3 = arr1.concat(arr2),
+    result = [];
+  for (let str of arr3) {
+    if (!result.includes(str)) {
+      result.push(str);
+    }
+  }
+  console.log(arr3);
+};
+myArr3();
+let myArr4 = () => {
+  let arr1 = [1, 2, 3, 4, 5, 6, 7],
+    arr2 = [4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15];
+  let arr11 = arr1.join();
+  let arr12 = arr2.join();
+  let arr13 = `${arr11},${arr12}`;
+  let arr14 = arr13.split(",");
+  console.log(arr13);
+  console.log(arr14);
+
+  let result1 = [];
+  for (let str of arr14) {
+    if (!result1.includes(str)) {
+      result1.push(str);
+    }
+  }
+
+  console.log(result1);
+};
+myArr4();
